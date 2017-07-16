aeb0e6012bad01d08fdf33db96076f4beb003c65
U201323409 <carlosdevelop@gmail.com>
2017-07-16 18:46:14 -0500

    Entidades Completas

--- Base de datos/Scritps/BaseDatos_GFRANUPC.sql
+++ Base de datos/Scritps/BaseDatos_GFRANUPC.sql
@@ -679,3 +679,46 @@ BEGIN CATCH
 	-- RAISERROR(@mensajeError, @codSeveridad, @codStatus)
 END CATCH
 GO
+
+-- DROP PROCEDURE [dbo].[USPS_ResultadoEvaluacion]
+CREATE PROCEDURE [dbo].[USPS_ResultadoEvaluacion]
+	@solicitudId	int,
+	@coderr			int OUT,
+	@msgerr			varchar(1000) OUT
+WITH ENCRYPTION
+AS
+BEGIN TRY
+
+	SELECT	RE.Id,
+			RE.SolicitudId,
+			S.NumSolicitud,
+			S.MontoCapital,
+			S.Estado,
+			RE.FechaReporte,
+			RE.ResultadoEjercicio,
+			RE.ErroresEncontrados
+	FROM Solicitud S
+	inner join Solicitante SL on S.id = SL.SolicitudId
+	inner join ReporteEvaluacion RE on S.Id = Re.SolicitudId
+	WHERE S.Id = @solicitudId
+	order by 1
+	
+	Set @coderr = 0
+	Set @msgerr = 'OK'
+
+END TRY
+BEGIN CATCH
+	DECLARE @mensajeError as varchar(4000)
+	DECLARE @codSeveridad as int
+	DECLARE @codStatus as int
+
+	Select @mensajeError = ERROR_MESSAGE(),
+		   @codSeveridad = ERROR_SEVERITY(),
+		   @codStatus = ERROR_STATE()
+	
+	set @coderr = 1
+	set @msgerr = @mensajeError
+
+	-- RAISERROR(@mensajeError, @codSeveridad, @codStatus)
+END CATCH
+GO
--- Fuentes/Web/SISGFRAN/UPC.SISGFRAN.EL/Inherited/SolicitudEL.cs
+++ Fuentes/Web/SISGFRAN/UPC.SISGFRAN.EL/Inherited/SolicitudEL.cs
@@ -14,6 +14,23 @@ namespace UPC.SISGFRAN.EL.Inherited
         public SolicitanteEL Solicitante { get; set; }
         public string NumSolicitud { get; set; }
         public DateTime FechaSolicitud { get; set; }
+        public string CiudadInteres { get; set; }
+        public string UbigeoCiudadInteres { get; set; }
+        public decimal MontoCapital { get; set; }
+        public string FuenteFinanciamiento { get; set; }
+        public DateTime FechaInicioOperacion { get; set; }
+        public Int16 LocalDisponible { get; set; }
+        public ParametroEL CondicionLocal { get; set; }
+        public string UbigeoLocal { get; set; }
+        public string DireccionLocal { get; set; }
+        public ParametroEL TipoUbicacion { get; set; }
+        public string ReferenciaComercial { get; set; }
+        public string ReferenciaBancaria { get; set; }
+        public ParametroEL Estado { get; set; }
+
+
+        public EntrevistaEL Entrevista { get; set; }
+        public ReporteEvaluacionEL ReporteEvaluacion { get; set; }
 
         public ListaPaginada<SolicitudEL> ListaSolicitudes { get; set; }
     }
