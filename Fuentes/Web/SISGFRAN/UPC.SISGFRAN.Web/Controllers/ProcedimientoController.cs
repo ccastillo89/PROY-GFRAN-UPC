ae747040b8da696f07e5ae3cebd19e67be41b620
U201323409 <carlosdevelop@gmail.com>
2017-08-28 02:50:39 -0500

    Controller CUS20 - Avance

---
+++ Fuentes/Web/SISGFRAN/UPC.SISGFRAN.Web/Controllers/ProcedimientoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UPC.SISGFRAN.Web.Controllers
+{
+    public class ProcedimientoController : Controller
+    {
+        //
+        // GET: /Procedimiento/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
