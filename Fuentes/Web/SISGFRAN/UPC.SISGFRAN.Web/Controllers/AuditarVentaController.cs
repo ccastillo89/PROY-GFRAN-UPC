d59fef029881f50958e63e2f23e5967b3a086804
U201323409 <carlosdevelop@gmail.com>
2017-09-15 00:16:36 -0500

    CUS - Auditar

---
+++ Fuentes/Web/SISGFRAN/UPC.SISGFRAN.Web/Controllers/AuditarVentaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UPC.SISGFRAN.Web.Controllers
+{
+    public class AuditarVentaController : Controller
+    {
+        //
+        // GET: /AuditarVenta/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
